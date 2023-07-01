fa071fc3285421ebb2dc83fbaef93ef080716229
Suthep Sangvirotjanaphat <suthep@central.tech>
2023-07-01 21:10:37 +0700

    DV-1 Upgrade .net version and Add a new option --plain

--- dotnet-ver/ListCommand.cs
+++ dotnet-ver/ListCommand.cs
@@ -8,69 +8,70 @@ using System.Xml.Linq;
 
 namespace dotnet_ver
 {
-  class ListCommand
-  {
-
-    [Option("-d|--directory", Description = "Start directory to be scanned for .csproj files. (default is current directory)")]
-    public string StartDirectory { get; } = null;
-
-
-    private int OnExecute()
-    { 
-      string currentDirectory = StartDirectory ?? Directory.GetCurrentDirectory();
-      var projectFiles = Directory.EnumerateFileSystemEntries(currentDirectory, "*.??proj", SearchOption.AllDirectories);
-
-      foreach (var projectFile in projectFiles)
-      {
-        var document = XDocument.Load(projectFile);
-        XElement projectNode;
-
-        Console.WriteLine(projectFile);
-
-        try
-        {
-          projectNode = document.GetElement("Project");
-        }
-        catch (Exception)
-        {
-          Console.WriteLine("  (Skipped. Not a .NET core project.)");
-          Console.WriteLine();
-          continue;
-        }
-
-        var sdk = projectNode?.Attribute("Sdk");
-        if (sdk == null)
-        {
-          Console.WriteLine("  (Skipped. Not a .NET core project.)");
-          Console.WriteLine();
-          continue;
-        }
-
-        GetVersion(projectNode, "Version");
-        GetVersion(projectNode, "FileVersion");
-        GetVersion(projectNode, "AssemblyVersion");
-        GetVersion(projectNode, "PackageVersion");
-        GetVersion(projectNode, "InformationVersion");
-          
-        Console.WriteLine();
-      }
-
-      return 0;
-    }
-
-    private static void GetVersion(XElement projectNode, string elementName)
+    class ListCommand
     {
-      if (projectNode == null) return;
 
-      string oldVersion = "";
-      var versionNode = projectNode
-                .Elements("PropertyGroup")
-                .SelectMany(it => it.Elements(elementName))
-                .SingleOrDefault();
+        [Option("-d|--directory", Description = @"Specifies the directory to begin scanning for .csproj files.
+            If not provided, the current working directory will be used.")]
+        public string StartDirectory { get; } = null;
 
-      oldVersion = versionNode?.Value; 
 
-      Console.WriteLine($"  {elementName,-20} = {oldVersion}"); 
+        private int OnExecute()
+        {
+            string currentDirectory = StartDirectory ?? Directory.GetCurrentDirectory();
+            var projectFiles = Directory.EnumerateFileSystemEntries(currentDirectory, "*.??proj", SearchOption.AllDirectories);
+
+            foreach (var projectFile in projectFiles)
+            {
+                var document = XDocument.Load(projectFile);
+                XElement projectNode;
+
+                Console.WriteLine(projectFile);
+
+                try
+                {
+                    projectNode = document.GetElement("Project");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("  (Skipped. Not a .NET core project.)");
+                    Console.WriteLine();
+                    continue;
+                }
+
+                var sdk = projectNode?.Attribute("Sdk");
+                if (sdk == null)
+                {
+                    Console.WriteLine("  (Skipped. Not a .NET core project.)");
+                    Console.WriteLine();
+                    continue;
+                }
+
+                GetVersion(projectNode, "Version");
+                GetVersion(projectNode, "FileVersion");
+                GetVersion(projectNode, "AssemblyVersion");
+                GetVersion(projectNode, "PackageVersion");
+                GetVersion(projectNode, "InformationVersion");
+
+                Console.WriteLine();
+            }
+
+            return 0;
+        }
+
+        private static void GetVersion(XElement projectNode, string elementName)
+        {
+            if (projectNode == null) return;
+
+            string oldVersion = "";
+            var versionNode = projectNode
+                      .Elements("PropertyGroup")
+                      .SelectMany(it => it.Elements(elementName))
+                      .SingleOrDefault();
+
+            oldVersion = versionNode?.Value;
+
+            Console.WriteLine($"  {elementName,-20} = {oldVersion}");
+        }
     }
-  }
 }
